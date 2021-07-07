2480291a41740baf5184c54caaf51f545a845c92
Benhur <tessele@weg.net>
2021-07-07 17:35:30 +0000

    implemented run_udevadm function

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
-/// #!/bin/bash
-/// 
-/// num_of_ifaces=0
-/// start_iface=100
-/// for devpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-///     syspath="${devpath%/dev}"
-///     devname="$(udevadm info -q name -p $syspath)"
-///     [[ "$devname" == "bus/"* ]] && continue
-///     eval "$(udevadm info -q property --export -p $syspath)"
-///     [[ -z "$ID_SERIAL" || "$ID_VENDOR" != "SimTech"* ]] && continue
-///     # echo "/dev/$devname $ID_VENDOR"
-///     devnum[${#devnum[*]}]=$ID_USB_INTERFACE_NUM
-///     num_of_ifaces=$((num_of_ifaces + 1))
-///     if [ $ID_USB_INTERFACE_NUM -lt $start_iface ]; then
-///         start_iface=$ID_USB_INTERFACE_NUM
-///     fi
-/// done
-/// #echo ${devnum[@]}
-/// echo $start_iface $num_of_ifaces
-/// if [ $num_of_ifaces -eq 0 ]; then
-///    exit 1
-/// fi
 use std::io;
+use std::env;
 use std::fs::{self};
 use std::path::Path;
+use std::process::Command;
+use std::str::from_utf8;
 
-struct USBDevs { path: &'static Path, filter: Vec<&'static str>, rec_limit: u32 }
+const USB_VENDOR_ID: (&str, usize) = ("ID_VENDOR_ID", 4);
+const USB_MODEL_ID: (&str, usize) = ("ID_MODEL_ID", 4);
+const USB_TTY_NUM: (&str, usize) = ("ID_USB_INTERFACE_NUM", 2);
+
+const SIMCOM: u32 = 7694; // 0x1e0e
+const SIM7600: u32 = 36865; // 0x9001
+
+
+struct USBDevs<'a> {
+    path: &'a Path,
+    filter: Vec<&'static str>,
+    rec_limit: u32
+}
+
+struct CommandResult {
+    status: Option<i32>,
+    stdout: Option<String>,
+    stderr: Option<String>
+}
 
 trait SysfsSearch {
     // Static method signature; `Self` refers to the implementor type.
@@ -35,28 +34,29 @@ trait SysfsSearch {
     }
 }
 
-impl USBDevs {
-    fn new(path: &'static Path, filter: Vec<&'static str>, rec_limit: u32) -> USBDevs {
+impl USBDevs<'_> {
+    fn new<'a>(path: &'a Path, filter: Vec<&'static str>, rec_limit: u32) -> USBDevs<'a> {
         USBDevs { path: path, filter: filter, rec_limit: rec_limit }
     }
 
-    fn visit_dirs(&mut self, dir: &Path, filter: &str, limit: u32, result: &mut Vec<String>) -> Result<(), io::Error> {
+    fn visit_dirs(dir: &Path, filter: &str, limit: u32, result: &mut Vec<String>) -> Result<(), io::Error> {
         if dir.is_dir() {
             for entry in fs::read_dir(dir)? {
                 let entry = entry?;
                 let path = entry.path();
+                // Here we need to consume the filename in order to skip dirs
                 let fname = entry.file_name();
-                let name = String::from(fname.to_str().unwrap());
-                if path.is_dir() &&
-                        !name.contains("subsystem") &&
-                        !name.contains("driver") &&
-                        !name.contains("firmware_node") &&
-                        !name.contains("port") &&
-                        limit > 0 {
-                    self.visit_dirs(&path.as_path(), filter, limit-1, result)?;
-                } else if name == String::from(filter) {
-                    let str_path = path.into_os_string();
-                    result.push(String::from(str_path.to_str().unwrap()));
+                let str_fname = String::from(fname.to_str().unwrap());
+                let condition = !str_fname.contains("subsystem") &&
+                    !str_fname.contains("driver") &&
+                    !str_fname.contains("firmware_node") &&
+                    !str_fname.contains("port") && limit > 0;
+                if path.is_dir() && condition {
+                    Self::visit_dirs(&path.as_path(), filter, limit-1, result)?;
+                } else if str_fname.as_str() == filter {
+                    let parent = path.parent().unwrap();
+                    let str_parent = parent.to_str().unwrap();
+                    result.push(String::from(str_parent)); // save the parent
                 }
             }
         }
@@ -71,23 +71,91 @@ impl USBDevs {
         println!("{:?} {:?}", self.path, self.filter);
         for i in 0..self.filter.len() {
             if i == 0 {
-                let _ = self.visit_dirs(self.path, self.filter[i], self.rec_limit, &mut result);
+                let _ = Self::visit_dirs(self.path, self.filter[i], self.rec_limit, &mut result);
             } else {
                 result.retain(|x| x.contains(self.filter[i]));
             }
         }
         result
     }
+
+    fn get_property(base_str: &str, key: (&str, usize)) -> Option<u32> {
+        let base_idx = base_str.find(key.0);
+        if base_idx.is_none() {
+            return None;
+        }
+        let base_idx = base_idx.unwrap();
+        let offset = key.0.len() + base_idx + 1;
+        if base_str.len() < offset + key.1 {
+            return None;
+        }
+        let value_sliced: &str = &base_str[offset..offset+key.1];
+        let value: u32 = u32::from_str_radix(value_sliced, 16).unwrap();
+        println!("{}, {}, {}", base_idx, value_sliced, value);
+        Some(value)
+    }
+
+    fn run_udevadm(&self, path_vec: &Vec<String>) {
+        let mut stdout_vec: Vec<CommandResult> = Vec::new();
+        for path in path_vec {
+            let opts = ["info", "-q", "property", "-p", path.as_str()];
+            stdout_vec.push(execute_command("udevadm", Some(&opts)));
+        }
+        let mut tty_vec: Vec<u32> = Vec::new();
+        for item in stdout_vec {
+            let stdout_str = item.stdout.unwrap();
+            if Self::get_property(&stdout_str, USB_VENDOR_ID) != Some(SIMCOM) {
+                continue;
+            }
+            if Self::get_property(&stdout_str, USB_MODEL_ID) != Some(SIM7600) {
+                continue;
+            }
+            let value = Self::get_property(&stdout_str, USB_TTY_NUM);
+            if value.is_none() {
+                continue;
+            };
+            tty_vec.push(value.unwrap());
+        }
+        let mut cloned_vec = tty_vec.clone();
+        let tty_vec_sorted = cloned_vec.as_mut_slice();
+        println!("TTY: {:?} {:?}", tty_vec, tty_vec_sorted);
+    }
+}
+
+fn execute_command(cmd: &str, args: Option<&[&str]>) -> CommandResult {
+    println!("DEB: {} {:?}", cmd, args);
+    let mut command = Command::new(cmd);
+    if args.is_some() {
+        command.args(args.unwrap());
+    }
+    let mut result = CommandResult{status: None, stdout: None, stderr: None};
+    let output = command.output().expect("ERR: Command failed!");
+    result.status = output.status.code();
+    if output.stderr.len() > 0 {
+        result.stderr = match from_utf8(output.stdout.as_slice()) {
+            Ok(v) => Some(v.to_string()),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+    if output.stdout.len() > 0 {
+        result.stdout = match from_utf8(output.stdout.as_slice()) {
+            Ok(v) => Some(v.to_string()),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+    result
 }
 
 fn main() {
-    let path = Path::new("/sys/bus/usb/devices/usb1");
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() < 2 {
+        println!("missing path argument");
+        return;
+    }
+    let path = Path::new(argv[1].as_str());
     let filter = vec!["dev", "ttyUSB"];
     let mut usbs: USBDevs = USBDevs::new(path, filter, 7);
     let result = usbs.search();
-    println!("\n--------------------------------------------------\n");
-    for item in result {
-        println!("{:?}", item);
-    }
+    usbs.run_udevadm(&result);
 }
 
